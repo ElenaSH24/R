01b90e314c0da92da5425143c671947be752f1f0
ElenaSH24 <elena@sh24.org.uk>
2022-02-03 09:23:50 +0000

    Update Orders_Totals_LabResults.R

--- Orders_Totals_LabResults.R
+++ Orders_Totals_LabResults.R
@@ -156,7 +156,7 @@ Summary1 <- rename(Summary1, Returned.Tests = Returns.x)
 
 # TO INPUT IN SUMMARY MANUALLY: TOT per Lab, based on 'LabResults' (change of methodology in June 2020, from 'LabReceipt' to 'LabResults') and for TDL and SPS separately----
 #Calculate overall TOT for current and previous month
-OverallTOTPrev <- OrdersMonth[(OrdersMonth$Lab.results.at.month.year == "2021-11" 
+OverallTOTPrev <- OrdersMonth[(OrdersMonth$Lab.results.at.month.year == "2021-12" 
                            & OrdersMonth$Area != "London"
                            & OrdersMonth$TOT.Bands != "0"),]
 table(OverallTOTPrev$TOT.Bands)
@@ -832,7 +832,7 @@ Summary93[is.na(Summary93)] <- "0"
 #Transpose the table at the end, after all calculations are done. Otherwise, calculations won't work in the columns, and you get lots of NAs----
 Summary93 = t(Summary93)
 
-write.table (Summary93, file="/Users/ElenaArdines1/Documents/Reports/1.Monthly_Reports/Performance_Reports/2021/2021_12/SummaryPerformance.2021.12.csv", col.names = F, row.names=T, sep=",")
+write.table (Summary93, file="/Users/ElenaArdines1/Documents/Reports/1.Monthly_Reports/Performance_Reports/2022/2022_01/SummaryPerformance.2022.01.csv", col.names = F, row.names=T, sep=",")
 
 ################################
 #clean up the environment
