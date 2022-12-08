4a9e8d7189bf9eecf6e28185f8a00ab36048c0c3
Elena <70645137+ElenaSH24@users.noreply.github.com>
2022-12-08 09:39:25 +0000

    Update RecodeArea function to removing Site
    This commit updates the RecodeArea function to remove the variable Site. We have tested that it works in November month end calculations, tallying with the adjustment in Tableau and the invoicing.

--- Recode_Area.R
+++ Recode_Area.R
@@ -1,124 +1,160 @@
 ###Create Function
 #DF = name of data frame
 #varname = variable that needs to be recoded (Area)
-#varname2 = variable that is used for conditioning (Site)
-#varname3 = second variable used for conditioning (LA.of.residence)
-#varname4 = third variable used for conditioning (Referred.from)
-#varname5 = variable used for conditioning (Default LA)
-recodeArea <- function(DF,varname,varname2,varname3,varname4,varname5){
+#varname1 = variable used for conditioning (Default LA)
+recodeArea <- function(DF,varname,varname1){
   DF[,varname] <- as.character(DF[,varname]) #change class to character
-  DF[,varname][(DF[,varname2]=="Beckenham Beacon Sexual Health Clinic"| 
-                  DF[,varname2]=="Burrel Street (SE London)"|
-                  DF[,varname2]=="Burrell Street" |
-                  DF[,varname2]=="Camberwell"|
-                  DF[,varname2]=="Waldron Health Centre")] <- "London"
   
-  DF[,varname][DF[,varname2]=="Howard House" & DF[,varname5]=="Bradford"] <- "Bradford"
-  DF[,varname][DF[,varname5]=="Bromley"] <- "Bromley"
-  DF[,varname][DF[,varname5]=="Buckinghamshire"] <- "Buckinghamshire"
+  DF[,varname][DF[,varname1]=="Bexley"] <- "Bexley"
+  DF[,varname][DF[,varname1]=="Blackburn with Darwen"] <- "Blackburn"
+  DF[,varname][DF[,varname1]=="Bradford"] <- "Bradford"
+  DF[,varname][DF[,varname1]=="Bromley"] <- "Bromley"
+  DF[,varname][DF[,varname1]=="Buckinghamshire"] <- "Buckinghamshire"
+  DF[,varname][DF[,varname1]=="Cheshire East"] <- "Cheshire East"
+  DF[,varname][DF[,varname1]=="Cornwall and Isles of Scilly PCT"] <- "Cornwall"
+  DF[,varname][DF[,varname1]=="County Durham"] <- "County Durham"
+  DF[,varname][DF[,varname1]=="Darlington"] <- "Darlington"
+  DF[,varname][DF[,varname1]=="Derby"] <- "Derby"
+  DF[,varname][DF[,varname1]=="Derbyshire"] <- "Derbyshire"
+  DF[,varname][DF[,varname1]=="Dorset"] <- "Dorset"
+  DF[,varname][DF[,varname1]=="East Berkshire"] <- "East Berkshire"
+  DF[,varname][DF[,varname1]=="Essex"] <- "Essex"
+  DF[,varname][DF[,varname1]=="Fettle"] <- "Fettle"
+  DF[,varname][DF[,varname1]=="Gateshead"] <- "Gateshead"
+  DF[,varname][DF[,varname1]=="Gilead"] <- "Gilead"
+  DF[,varname][DF[,varname1]=="Glasgow"] <- "Glasgow"
+  DF[,varname][DF[,varname1]=="Gogodoc"] <- "Gogodoc"
+  DF[,varname][DF[,varname1]=="Greater Manchester"] <- "Greater Manchester"
+  DF[,varname][DF[,varname1]=="Halton"] <- "Halton"
+  DF[,varname][DF[,varname1]=="Herefordshire"] <- "Herefordshire"
+  DF[,varname][DF[,varname1]=="Hertfordshire"] <- "Hertfordshire"
+  DF[,varname][DF[,varname1]=="Hillingdon"] <- "Hillingdon"
+  DF[,varname][DF[,varname1]=="Kirklees"] <- "Kirklees"
+  DF[,varname][DF[,varname1]=="Knowsley"] <- "Knowsley"
+  DF[,varname][DF[,varname1]=="Leicester"] <- "Leicester"
+  DF[,varname][DF[,varname1]=="Leicestershire"] <- "Leicestershire"
+  DF[,varname][DF[,varname1]=="Liverpool"] <- "Liverpool"
+  DF[,varname][DF[,varname1]=="Medway"] <- "Medway"
+  DF[,varname][DF[,varname1]=="MTV"] <- "MTV event"
+  DF[,varname][DF[,varname1]=="North Staffordshire"] <- "North Staffordshire"
+  DF[,varname][DF[,varname1]=="Nottingham"] <- "Nottingham"
+  DF[,varname][DF[,varname1]=="PHE"] <- "PHE"
+  DF[,varname][DF[,varname1]=="Romania"] <- "Romania"
+  DF[,varname][DF[,varname1]=="Rotherham"] <- "Rotherham"
+  DF[,varname][DF[,varname1]=="Rutland"] <- "Rutland"
+  DF[,varname][DF[,varname1]=="Shropshire"] <- "Shropshire"
+  DF[,varname][DF[,varname1]=="South Tyneside"] <- "South Tyneside"
+  DF[,varname][DF[,varname1]=="Southend-on-Sea"] <- "Southend"
+  DF[,varname][DF[,varname1]=="Staffordshire"] <- "Staffordshire"
+  DF[,varname][DF[,varname1]=="Stockport"] <- "Stockport"
+  DF[,varname][DF[,varname1]=="Stoke-on-Trent"] <- "Stoke"
+  DF[,varname][DF[,varname1]=="Sunderland"] <- "Sunderland"
+  DF[,varname][DF[,varname1]=="Tameside"] <- "Tameside"
+  DF[,varname][DF[,varname1]=="Telford and Wrekin"] <- "Telford and Wrekin"
+  DF[,varname][DF[,varname1]=="Thurrock"] <- "Thurrock"
+  DF[,varname][DF[,varname1]=="Warrington"] <- "Warrington"
+  DF[,varname][DF[,varname1]=="Wirral"] <- "Wirral"
+  DF[,varname][DF[,varname1]=="Worcestershire"] <- "Worcestershire"
   
-  DF[,varname][(DF[,varname2]=="Axess Sexual Health Crewe" | DF[,varname2]=="Axess Sexual Health Macclesfield") 
-               & (DF[,varname5]=="Cheshire East")] <- "Cheshire East"
+  DF[,varname][DF[,varname1]=="Oldham" | DF[,varname1]=="Rochdale" | DF[,varname1]=="Bury"] <- "Orbish"
   
-  DF[,varname][(DF[,varname5]=="Cornwall and Isles of Scilly PCT")] <- "Cornwall"
-  DF[,varname][DF[,varname5]=="County Durham"] <- "County Durham"
-  #DF[,varname][DF[,varname2]=="Darlington GUM Clinic" & DF[,varname3]=="Darlington"] <- "Darlington"
-  DF[,varname][(DF[,varname5]=="Darlington")] <- "Darlington"
-  DF[,varname][DF[,varname2]=="London Road Community Hospital"] <- "Derby"
-  DF[,varname][DF[,varname2]=="Wheatbridge Clinic"] <- "Derbyshire"
-  DF[,varname][(DF[,varname5]=="Dorset")] <- "Dorset"
-  DF[,varname][(DF[,varname5]=="East Berkshire")] <- "East Berkshire"
-  DF[,varname][DF[,varname2]=="Essex Hub"] <- "Essex"
-  DF[,varname][DF[,varname2]=="Trinity Square Health Centre"] <- "Gateshead"
-  DF[,varname][DF[,varname5]=="Glasgow"] <- "Glasgow"
-  DF[,varname][(DF[,varname5]=="Halton")] <- "Halton"
-  DF[,varname][DF[,varname2]=="iSH Hereford"] <- "Herefordshire"
-  DF[,varname][DF[,varname2]=="Hertford Clinic"] <- "Hertfordshire"
-  DF[,varname][DF[,varname5]=="Hillingdon"] <- "Hillingdon"
+  DF[,varname][(DF[,varname1]=="Lambeth"| DF[,varname1]=="Lewisham"| DF[,varname1]=="Southwark")] <- "London"
   
-  DF[,varname][(DF[,varname5]=="Kirklees")] <- "Kirklees"
-  DF[,varname][DF[,varname5]=="Knowsley"] <- "Knowsley"
+  DF[,varname][DF[,varname1]=="Northern Ireland Belfast PCT" |
+                 DF[,varname1]=="Northern Ireland Northern PCT" |
+                 DF[,varname1]=="Northern Ireland South Eastern PCT" |
+                 DF[,varname1]=="Northern Ireland Southern PCT" |
+                 DF[,varname1]=="Northern Ireland Western PCT" | 
+                 DF[,varname1]=="Northern Ireland Offline: Southern"] <- "Northern Ireland"
   
-  DF[,varname][(DF[,varname5]=="Leicester")] <- "Leicester"
-  DF[,varname][(DF[,varname5]=="Leicestershire")] <- "Leicestershire"
+  DF[,varname][DF[,varname1]=="Ireland - Dublin" |  DF[,varname1]=="Ireland - Cork" |
+                 DF[,varname1]=="Ireland - Kerry" | DF[,varname1]=="Ireland - Wicklow" | 
+                 DF[,varname1]=="Ireland - Kildare" | DF[,varname1]=="Ireland - Cavan" | 
+                 DF[,varname1]=="Ireland - Louth" | DF[,varname1]=="Ireland - Meath" |
+                 DF[,varname1]=="Ireland - Monaghan" | DF[,varname1]=="Ireland - Offaly" |
+                 DF[,varname1]=="Ireland - Galway" | DF[,varname1]=="Ireland - Mayo" |
+                 DF[,varname1]=="Ireland - Laois" | DF[,varname1]=="Ireland - Roscommon" |
+                 DF[,varname1]=="Ireland - Donegal" | DF[,varname1]=="Ireland - Sligo" |
+                 DF[,varname1]=="Ireland - Leitrim" | DF[,varname1]=="Ireland - Limerick" |
+                 DF[,varname1]=="Ireland - Clare" | DF[,varname1]=="Ireland - Tipperary" |
+                 DF[,varname1]=="Ireland - Carlow" | DF[,varname1]=="Ireland - Kilkenny" | 
+                 DF[,varname1]=="Ireland - Westmeath" | DF[,varname1]=="Ireland - Longford" |
+                 DF[,varname1]=="Ireland - Waterford" | DF[,varname1]=="Ireland - Wexford"] <- "Ireland"
   
-  DF[,varname][DF[,varname5]=="Liverpool"] <- "Liverpool"
-  DF[,varname][DF[,varname2]=="Clover Street Sexual Health Clinic"] <- "Medway"
-  DF[,varname][DF[,varname2]=="Cobridge Sexual Health Clinic"] <- "North Staffordshire"
+  DF[,varname][ DF[,varname1]=="PrEP Trial - Barnsley Integrated Sexual Health" |
+                  DF[,varname1]=="PrEP Trial - Bishop Auckland General Hospital" |
+                  DF[,varname1]=="PrEP Trial - Boston Sexual Health Clinic" |
+                  DF[,varname1]=="PrEP Trial - Coventry & Warwickshire Hospital" |
+                  DF[,varname1]=="PrEP Trial - King's Mill Hospital" |
+                  DF[,varname1]=="PrEP Trial - Kingston Hospital" |
+                  DF[,varname1]=="PrEP Trial - Lindon House Sexual Health Clinic" |
+                  DF[,varname1]=="PrEP Trial - Long Eaton Health Centre (Integrated)" |
+                  DF[,varname1]=="PrEP Trial - Sexual Health @ London Road" |
+                  DF[,varname1]=="PrEP Trial - St Helier Hospital (C&W Integrated)" |
+                  DF[,varname1]=="PrEP Trial - St Marks Hospital" |
+                  DF[,varname1]=="PrEP Trial - University Hospital Birmingham" |
+                  DF[,varname1]=="PrEP Trial - Wakefield Integrated Sexual Health Services"] <- "PrEP Trial"
   
-  DF[,varname][DF[,varname5]=="Northern Ireland Belfast PCT" |
-                 DF[,varname5]=="Northern Ireland Northern PCT" |
-                 DF[,varname5]=="Northern Ireland South Eastern PCT" |
-                 DF[,varname5]=="Northern Ireland Southern PCT" |
-                 DF[,varname5]=="Northern Ireland Western PCT" | 
-                 DF[,varname5]=="Northern Ireland Offline: Southern"] <- "Northern Ireland"
-  
-  DF[,varname][DF[,varname5]=="Ireland - Dublin" |  DF[,varname5]=="Ireland - Cork" |
-                 DF[,varname5]=="Ireland - Kerry" | DF[,varname5]=="Ireland - Wicklow" | 
-                 DF[,varname5]=="Ireland - Kildare" | DF[,varname5]=="Ireland - Cavan" | 
-                 DF[,varname5]=="Ireland - Louth" | DF[,varname5]=="Ireland - Meath" |
-                 DF[,varname5]=="Ireland - Monaghan" | DF[,varname5]=="Ireland - Offaly" |
-                 DF[,varname5]=="Ireland - Galway" | DF[,varname5]=="Ireland - Mayo" |
-                 DF[,varname5]=="Ireland - Laois" | DF[,varname5]=="Ireland - Roscommon" |
-                 DF[,varname5]=="Ireland - Donegal" | DF[,varname5]=="Ireland - Sligo" |
-                 DF[,varname5]=="Ireland - Leitrim" | DF[,varname5]=="Ireland - Limerick" |
-                 DF[,varname5]=="Ireland - Clare" | DF[,varname5]=="Ireland - Tipperary" |
-                 DF[,varname5]=="Ireland - Carlow" | DF[,varname5]=="Ireland - Kilkenny" | 
-                 DF[,varname5]=="Ireland - Westmeath" | DF[,varname5]=="Ireland - Longford" |
-                 DF[,varname5]=="Ireland - Waterford" | DF[,varname5]=="Ireland - Wexford"
-               ] <- "Ireland"
-
-  DF[,varname][DF[,varname5]=="Nottingham"] <- "Nottingham"
-  DF[,varname][DF[,varname2]=="ARAS Bucuresti"] <- "Romania"
-  
-  DF[,varname][(DF[,varname5]=="Rutland")] <- "Rutland"
- 
-  DF[,varname][DF[,varname3]=="Shropshire" | DF[,varname5]=="Shropshire"] <- "Shropshire"
-  DF[,varname][DF[,varname5]=="South Tyneside"] <- "South Tyneside"
-  DF[,varname][DF[,varname2]=="Cannock Chase Hospital"|DF[,varname2]=="Stafford Central Clinic"] <- "Staffordshire"
-  DF[,varname][DF[,varname5]=="Stoke-on-Trent"] <- "Stoke"
-  DF[,varname][DF[,varname5]=="Sunderland"] <- "Sunderland"
-  DF[,varname][DF[,varname5]=="Telford and Wrekin"] <- "Telford and Wrekin"
-  DF[,varname][(DF[,varname5]=="Warrington")] <- "Warrington"
-  DF[,varname][DF[,varname2]=="Worcestershire Hub"] <- "Worcestershire"
-  DF[,varname][(DF[,varname5]=="Southend-on-Sea")] <- "Southend"
-  DF[,varname][(DF[,varname5]=="Blackburn with Darwen")] <- "Blackburn"
-  
-  DF[,varname][DF[,varname2]=="Fettle Hub"] <- "Fettle"
-  DF[,varname][DF[,varname2]=="Gilead"] <- "Gilead"
-  DF[,varname][DF[,varname2]=="MTV event"] <- "MTV event"
-  DF[,varname][(DF[,varname5]=="Gogodoc")] <- "Gogodoc"
-  
-  DF[,varname][(DF[,varname2]=="Freetesting.hiv Hub" & (DF[,varname5]!="PHE"))] <- "Freetesting.hiv"
-  DF[,varname][DF[,varname5]=="PHE"] <- "PHE"
-  
-  DF[,varname][DF[,varname5]=="PrEP Trial - Boston Sexual Health Clinic" |
-                 DF[,varname5]=="PrEP Trial - King's Mill Hospital" |
-                 DF[,varname5]=="PrEP Trial - Kingston Hospital" |
-                 DF[,varname5]=="PrEP Trial - Lindon House Sexual Health Clinic" |
-                 DF[,varname5]=="PrEP Trial - Wakefield Integrated Sexual Health Services" |
-                 DF[,varname5]=="PrEP Trial - St Marks Hospital" |
-                 DF[,varname5]=="PrEP Trial - Barnsley Integrated Sexual Health" |
-                 DF[,varname5]=="PrEP Trial - St Helier Hospital (C&W Integrated)" |
-                 DF[,varname5]=="PrEP Trial - Bishop Auckland General Hospital" |
-                 DF[,varname5]=="PrEP Trial - Coventry & Warwickshire Hospital" |
-                 DF[,varname5]=="PrEP Trial - Long Eaton Health Centre (Integrated)" |
-                 DF[,varname5]=="PrEP Trial - University Hospital Birmingham" |
-                 DF[,varname5]=="PrEP Trial - Sexual Health @ London Road"] <- "PrEP Trial"
-  
-  DF[,varname][DF[,varname5]=="Bexley"] <- "Bexley"
-  DF[,varname][DF[,varname5]=="Thurrock"] <- "Thurrock"
-  DF[,varname][DF[,varname5]=="Wirral"] <- "Wirral"
-  DF[,varname][DF[,varname5]=="Greater Manchester"] <- "Greater Manchester"
-  DF[,varname][DF[,varname5]=="Stockport"] <- "Stockport"
-  DF[,varname][DF[,varname5]=="Tameside"] <- "Tameside"
-  DF[,varname][DF[,varname5]=="Rotherham"] <- "Rotherham"
-  
+  DF[,varname][ DF[,varname1]=="Freetesting - Bedford" |
+                  DF[,varname1]=="Freetesting - Blackburn with Darwen" |
+                  DF[,varname1]=="Freetesting - Blackpool" |
+                  DF[,varname1]=="Freetesting - Bolton" |
+                  DF[,varname1]=="Freetesting - Brighton and Hove" |
+                  DF[,varname1]=="Freetesting - Bury" |
+                  DF[,varname1]=="Freetesting - Camden" |
+                  DF[,varname1]=="Freetesting - Central Bedfordshire" |
+                  DF[,varname1]=="Freetesting - County Durham" |
+                  DF[,varname1]=="Freetesting - Coventry" |
+                  DF[,varname1]=="Freetesting - Darlington" |
+                  DF[,varname1]=="Freetesting - Doncaster" |
+                  DF[,varname1]=="Freetesting - Gateshead" |
+                  DF[,varname1]=="Freetesting - Hampshire" |
+                  DF[,varname1]=="Freetesting - Hartlepool" |
+                  DF[,varname1]=="Freetesting - Hertfordshire" |
+                  DF[,varname1]=="Freetesting - Islington" |
+                  DF[,varname1]=="Freetesting - Kent" |
+                  DF[,varname1]=="Freetesting - Kingston upon Thames" |
+                  DF[,varname1]=="Freetesting - Lancashire" |
+                  DF[,varname1]=="Freetesting - Leeds" |
+                  DF[,varname1]=="Freetesting - Leicester" |
+                  DF[,varname1]=="Freetesting - Leicestershire" |
+                  DF[,varname1]=="Freetesting - Manchester" |
+                  DF[,varname1]=="Freetesting - Middlesbrough" |
+                  DF[,varname1]=="Freetesting - Milton Keynes" |
+                  DF[,varname1]=="Freetesting - Newcastle upon Tyne" |
+                  DF[,varname1]=="Freetesting - Norfolk" |
+                  DF[,varname1]=="Freetesting - North Tyneside" |
+                  DF[,varname1]=="Freetesting - Northumberland" |
+                  DF[,varname1]=="Freetesting - Nottingham" |
+                  DF[,varname1]=="Freetesting - Nottinghamshire" |
+                  DF[,varname1]=="Freetesting - Oldham" |
+                  DF[,varname1]=="Freetesting - Oxfordshire" |
+                  DF[,varname1]=="Freetesting - Portsmouth" |
+                  DF[,varname1]=="Freetesting - Redbridge" |
+                  DF[,varname1]=="Freetesting - Redcar and Cleveland" |
+                  DF[,varname1]=="Freetesting - Richmond upon Thames" |
+                  DF[,varname1]=="Freetesting - Rochdale" |
+                  DF[,varname1]=="Freetesting - Rotherham" |
+                  DF[,varname1]=="Freetesting - Salford" |
+                  DF[,varname1]=="Freetesting - South Tyneside" |
+                  DF[,varname1]=="Freetesting - Southampton" |
+                  DF[,varname1]=="Freetesting - Stockport" |
+                  DF[,varname1]=="Freetesting - Stockton-on-Tees" |
+                  DF[,varname1]=="Freetesting - Suffolk" |
+                  DF[,varname1]=="Freetesting - Sunderland" |
+                  DF[,varname1]=="Freetesting - Swindon" |
+                  DF[,varname1]=="Freetesting - Tameside" |
+                  DF[,varname1]=="Freetesting - Telford and Wrekin" |
+                  DF[,varname1]=="Freetesting - Trafford" |
+                  DF[,varname1]=="Freetesting - Wakefield" |
+                  DF[,varname1]=="Freetesting - Wandsworth" |
+                  DF[,varname1]=="Freetesting - Warwickshire" |
+                  DF[,varname1]=="Freetesting - West Sussex" |
+                  DF[,varname1]=="Freetesting - Wigan"]  <- "Freetesting.hiv"
   
   DF[,varname] <- as.factor(DF[,varname])  #change class back to factor
   DF[,varname] #prints out variable 
 }
-
 # DF[,varname][DF[,varname5]==(grepl("PrEP Trial", DF$varname5))] <- "PrEP Trial"
 
 #once the function is done, save it on a script.
@@ -126,6 +162,7 @@ recodeArea <- function(DF,varname,varname2,varname3,varname4,varname5){
 #source("file.path")
 
 
+# ------------------------------------------------------------
 # DF = name of data frame // varname = variable that needs to be recoded (Area) // varname2 = variable that is used for conditioning (Region)
 recodeContraception <- function(DF,varname,varname2){
   DF[,varname] <- as.character(DF[,varname]) #change class to character
@@ -171,7 +208,7 @@ recodeContraception <- function(DF,varname,varname2){
   DF[,varname][DF[,varname2]=="Wirral"] <- "Wirral"
   
   DF[,varname][DF[,varname2]=="Oldham" | DF[,varname2]=="Rochdale" | DF[,varname2]=="Bury"] <- "Orbish"
- 
+  
   DF[,varname] <- as.factor(DF[,varname])  #change class back to factor
   DF[,varname] #prints out variable 
 }
